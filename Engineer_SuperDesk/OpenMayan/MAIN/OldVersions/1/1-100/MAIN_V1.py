e4e842f475101f8bace25a9bcca8e52ce6d47fd2
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-27 16:05:57 -0700

    MAIN_V1.py
    An archive of the first version of the Main script for this directory.

---
+++ Engineer_SuperDesk/OpenMayan/MAIN/OldVersions/1/1-100/MAIN_V1.py
@@ -0,0 +1,6 @@
+# Start of script
+""" Info
+This is the place for OpenMayan to be hosted, as it is the 3D graphics engine that AutoDesk uses. This is an open-source fork.
+"""
+print ("This section is not complete yet")
+# End of script
