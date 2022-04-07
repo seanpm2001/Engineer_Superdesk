0afcc8f7a4d1f5c230994d0b9c95bf4604f9a33d
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-06 18:08:44 -0700

    White_Sand_Default_V1.glsl
    An archive of the first version of the white sand texture file for this project.

---
+++ Architect/LIBraries/Materials/Earth/Sand/White/Default/!OldVersions/1/1-100/White_Sand_Default_V1.glsl
@@ -0,0 +1,99 @@
+// Start of script
+/* Material file for the Engineer Superdesk Architect
+* White sand (Default)
+* Categories: sand, natural, earth, beach, crushed, crush, white, grain
+* Flammable: no
+* Renewable: no
+* Chemical element: false
+* Chemical element symbol: Nil
+* Atomic number: nil
+* Phase: solid
+* Rare: no
+* Related: other types of sand, gravel
+* Type: Sand (white)
+* Melting point: Unknown
+* Can obtain rust: false
+*/
+// Sample file, stock file from https://github.com/libretro/glsl-shaders/blob/master/stock.glsl
+#if defined(VERTEX)
+#if __VERSION__ >= 130
+#define COMPAT_VARYING out
+#define COMPAT_ATTRIBUTE in
+#define COMPAT_TEXTURE texture
+#else
+#define COMPAT_VARYING varying 
+#define COMPAT_ATTRIBUTE attribute 
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+COMPAT_ATTRIBUTE vec4 VertexCoord;
+COMPAT_ATTRIBUTE vec4 COLOR;
+COMPAT_ATTRIBUTE vec4 TexCoord;
+COMPAT_VARYING vec4 COL0;
+COMPAT_VARYING vec4 TEX0;
+
+uniform mat4 MVPMatrix;
+uniform COMPAT_PRECISION int FrameDirection;
+uniform COMPAT_PRECISION int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+
+void main()
+{
+    gl_Position = VertexCoord.x * MVPMatrix[0] + VertexCoord.y * MVPMatrix[1] + VertexCoord.z * MVPMatrix[2] + VertexCoord.w * MVPMatrix[3];
+    TEX0.xy = TexCoord.xy;
+}
+
+#elif defined(FRAGMENT)
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING in
+#define COMPAT_TEXTURE texture
+out vec4 FragColor;
+#else
+#define COMPAT_VARYING varying
+#define FragColor gl_FragColor
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+#else
+precision mediump float;
+#endif
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+uniform COMPAT_PRECISION int FrameDirection;
+uniform COMPAT_PRECISION int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+uniform sampler2D Texture;
+COMPAT_VARYING vec4 TEX0;
+
+void main()
+{
+    FragColor = COMPAT_TEXTURE(Texture, TEX0.xy);
+}
+void white_sand_default_Texture()
+{
+  // Not available yet
+}
+#endif
+/* File info
+* File type: GLSL source file (*.glsl)
+* File version: 1 (2022, Wednesday, April 6th at 6:08 pm)
+* Line count (including blank lines and compiler line): 100
+*/
+// End of script
