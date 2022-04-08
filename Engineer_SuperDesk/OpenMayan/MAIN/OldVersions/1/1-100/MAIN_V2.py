b051719b3d12f80737f786585c0ede19d47cdb03
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-07 19:12:35 -0700

    MAIN_V2.py
    An archive of the 2nd version of the main script for OpenMayan.

---
+++ Engineer_SuperDesk/OpenMayan/MAIN/OldVersions/1/1-100/MAIN_V2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# Start of script
+""" Info
+This is the place for OpenMayan to be hosted, as it is the 3D graphics engine that AutoDesk uses. This is an open-source fork.
+The AutoDesk variant is called "Maya" this variant is called "Mayan"
+You can't copyright a civilization that has been gone for over a millenia, so I assume this is fine
+"""
+# Header
+class mayan()
+import random
+import gui
+import os
+# Functions
+def mayanAbout():
+  print("About Engineer Superdesk: Mayan")
+  print("Version 0.02 - 2022, Thursday, April 7th at 7:12 pm PST")
+  print("'An open source recreation of AutoDesk Maya'")
+  break
+def main():
+  # Coming soon
+  print ("This section is not complete yet")
+  break
+def scriptsOnLoad():
+  # Coming soon
+  import null
+  break
+def texturesOnLoad():
+  # Coming soon
+  break
+
+# Start prompt
+return mayanAbout()
+return main()
+return scriptsOnLoad()
+return texturesOnLOad()
+
+# Exit prompt
+noMore = input("Press [ENTER] key to quit")
+break
+
+''' File info
+File type: Python source file (*.py)
+File version: 2 (2022, Thursday, April 7th at 7:12 pm PST)
+Line count (including blank lines and compiler line): 48
+'''
+
+# End of script
